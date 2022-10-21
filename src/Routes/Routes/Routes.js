f492feb1928ba5f0d3a9869264739b0c5887d6a8
mostafizurRahaman <fahim654326@gmail.com>
2022-10-22 01:01:42 +0600

    serverside api link changeed

--- src/Routes/Routes/Routes.js
+++ src/Routes/Routes/Routes.js
@@ -11,52 +11,71 @@ import PrivateRoute from "../PrivateRoute/PrivateRoute";
 
 export const routes = createBrowserRouter([
    {
-      path: '/', 
-      element: <Main></Main>, 
+      path: "/",
+      element: <Main></Main>,
       children: [
          {
-            path: '/', 
+            path: "/",
             element: <Home></Home>,
-            loader: ()=> fetch('http://localhost:5000/news')
-         }, 
-         {
-            path: '/home', 
-            element: <Home></Home>,
-            loader: ()=> fetch('http://localhost:5000/news')
-         }, 
-         {
-            path: '/category/:id', 
-            element: <PrivateRoute><Category></Category></PrivateRoute>, 
-            loader: async({params})=>{
-               return fetch(`http://localhost:5000/category/${params.id}`);
-            }
-         }, 
-         {
-            path: "/news/:id", 
-            element: <PrivateRoute><News></News></PrivateRoute>, 
-            loader: ({params})=> fetch(`http://localhost:5000/news/${params.id}`)
-         }, 
-         {
-            path: '/login',
-            element: <Login></Login>
-         }, 
-         {
-            path: '/register', 
-            element: <Register></Register>
-         }, 
-         {
-            path:'/terms', 
-            element: <TermsAndServices></TermsAndServices>
+            loader: () =>
+               fetch(" https://dragron-news-severside.vercel.app/news"),
          },
          {
-            path:'/profile', 
-            element: <PrivateRoute> <Profile></Profile> </PrivateRoute>
+            path: "/home",
+            element: <Home></Home>,
+            loader: () =>
+               fetch(" https://dragron-news-severside.vercel.app/news"),
          },
          {
-           path: '*', 
-           element: <h1>Not found</h1>
-         }
-      ]
-   }
-   
-])
+            path: "/category/:id",
+            element: (
+               <PrivateRoute>
+                  <Category></Category>
+               </PrivateRoute>
+            ),
+            loader: async ({ params }) => {
+               return fetch(
+                  ` https://dragron-news-severside.vercel.app/category/${params.id}`
+               );
+            },
+         },
+         {
+            path: "/news/:id",
+            element: (
+               <PrivateRoute>
+                  <News></News>
+               </PrivateRoute>
+            ),
+            loader: ({ params }) =>
+               fetch(
+                  ` https://dragron-news-severside.vercel.app/news/${params.id}`
+               ),
+         },
+         {
+            path: "/login",
+            element: <Login></Login>,
+         },
+         {
+            path: "/register",
+            element: <Register></Register>,
+         },
+         {
+            path: "/terms",
+            element: <TermsAndServices></TermsAndServices>,
+         },
+         {
+            path: "/profile",
+            element: (
+               <PrivateRoute>
+                  {" "}
+                  <Profile></Profile>{" "}
+               </PrivateRoute>
+            ),
+         },
+         {
+            path: "*",
+            element: <h1>Not found</h1>,
+         },
+      ],
+   },
+]);
